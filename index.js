248e8d951a0e6c04feaf5770c939de122f78c5fa
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-07 15:32:34 -0800

    completed getDataFromAPI function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-//get data from API
-function getDataFromAPI(){
 
+const youTube_url= "https://www.googleapis.com/youtube/v3/search";
+
+//get data from API
+function getDataFromAPI(searchTerm, callback){
+  //send query to API
+  const settings = {
+    URL: "youTube_url",
+    data: {
+      q: `${searchTerm} in:name`,
+      per_page: 5
+    },
+    dataType: "json"
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
 }
 
 //render results
 function renderResults(){
-
+  //append results to HMTL
 }
 
 //display search data
@@ -15,5 +29,5 @@ function displaySearchData(){
 
 //watch for click on submit button
 function watchForSubmit(){
-  
+
 }
