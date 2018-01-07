14987a6e7f1e6e3d438982da686250d3c16ae75b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-07 14:46:17 -0800

    input function stubs

---
+++ index.js
@@ -0,0 +1,19 @@
+//get data from API
+function getDataFromAPI(){
+
+}
+
+//render results
+function renderResults(){
+
+}
+
+//display search data
+function displaySearchData(){
+
+}
+
+//watch for click on submit button
+function watchForSubmit(){
+  
+}
