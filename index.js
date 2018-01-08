47528644191219adf7ab5eb831b9f7aacc978ef7
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-07 21:55:10 -0800

    wrote function to get data from YouTube API

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const youTube_url= "https://www.googleapis.com/youtube/v3/search";
 //get data from API
 function getDataFromAPI(searchTerm, callback){
   //send query to API
-  const settings = {
-    URL: "youTube_url",
+console.log('start of getDataFromAPI working');
+
+  const querySettings = {
     part: 'snippet',
-    key: 'AIzaSyAgn_oFoGo-8EOWZ4yh-v_NCF3MuiziDPg'
-    data: {
-      q: `${searchTerm}`,
-      per_page: 5
-    },
-    dataType: "json"
-    type: 'GET',
-    success: callback
+    key: "AIzaSyAgn_oFoGo-8EOWZ4yh-v_NCF3MuiziDPg",
+    q: `${searchTerm}`,
+    maxResults: 25,
+    type: 'video',
   };
-  $.ajax(settings);
+  $.getJSON(youTube_url, querySettings, callback);
+  console.log('end of getDataFromAPI working');
+  console.log($.getJSON(youTube_url, querySettings, callback));
 }
 
 //render results
-function renderResults(){
+/*function renderResults(result){
   //append results to HMTL
-  $('.js-results').append()
-}
+  $('.js-results').append(`<video src="${result.items.snippet.thumbnails.medium.url}"></video>`);
+}*/
 
 //display search data
 function displaySearchData(){
@@ -32,7 +31,17 @@ function displaySearchData(){
 
 //watch for click on submit button
 function watchForSubmit(){
+  $(".js-submit-search").submit(function(){
+      event.preventDefault();
 
+      const query = $(this).prev()
+      getDataFromAPI(query, needToChangeThis);
+  });
 }
 
-$(getDataFromAPI);
+function docReady(){
+  getDataFromAPI();
+  watchForSubmit();
+}
+
+$(docReady);
